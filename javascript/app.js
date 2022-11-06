994dfe25cc527c522f659bf9000af624bfcb398a
escheive <scheiveerik@gmail.com>
2022-11-06 14:25:43 -0800

    added an embedded video on idex html

--- javascript/app.js
+++ javascript/app.js
@@ -62,6 +62,63 @@ const load = () => {
 
 
 
+
+// Resume on index html
+
+// grab the resume titles for a clickable buttons to open and close sections
+const resume = document.body.querySelector('#myResume')
+const education = document.body.querySelector('#education')
+const experience = document.body.querySelector('#experience')
+
+// grab the resume container so that i can hide and display it
+let resumeDisplay = document.querySelector('#resume')
+
+// grab the sections of education so that i can hide and display
+let educationSections = document.querySelectorAll('.educationSection')
+
+// track if resume is open or closed
+let resumeOpen = 'closed'
+// track if education and experience section of resume is open
+let educationOpen = 'closed'
+let experienceOpen = 'closed'
+
+// function to display and close resume
+const openResume = () => {
+    if(resumeOpen === 'opened'){
+        resumeDisplay.style.display = 'none'
+        resumeOpen = 'closed'
+    }else{
+        resumeDisplay.style.display = 'block'
+        resumeOpen = 'opened'
+    }
+}
+//
+const openEducation = () => {
+    if(educationOpen === 'opened'){
+        for(i=0; i < educationSections.length; i++){
+            educationSections[i].style.display = 'none';
+            educationOpen = 'closed';
+        }
+    }else{
+        for(i=0; i < educationSections.length; i++){
+            educationSections.style.display = 'block';
+            educationOpen = 'opened';
+        }
+    }
+}
+//
+const openExperience = () => {
+    if(experienceOpen === 'opened'){
+        experience.style.display = 'none'
+        experienceOpen = 'closed'
+    }else{
+        experience.style.display = 'block'
+        experienceOpen = 'opened'
+    }
+}
+
+
+
 // Dark Mode on index html
 
 // grab title text which will be button
@@ -77,24 +134,33 @@ const darkMode = () => {
         for(i=0; i < li.length; i++){
             li[i].style.color="white";
         }
-        body.style.background="black";
+        darkModeTitle.style['-webkit-text-stroke']="1px white";
+        resume.style.color="white";
+        resumeDisplay.style.color="white";
+        body.style.background="grey";
         theme = "dark"
     }else{
         for(i=0; i < li.length; i++){
             li[i].style.color="black";
         }
+        darkModeTitle.style['-webkit-text-stroke']="1px black";
+        resume.style.color="black";
+        resumeDisplay.style.color="black";
         body.style.background="moccasin";
         theme = "light"
     }
 }
 
 
-
 // event listeners
-// nextBtn.addEventListener('click', carouselNext)
 
-// prevBtn.addEventListener('click', carouselBack)
-
+// for page load to start carousel on about me page
 window.addEventListener('load', load);
-
+// for clicking on my name on index html to togle darkmode
 darkModeTitle.addEventListener('click', darkMode)
+// for toggling my resume
+resume.addEventListener('click', openResume)
+// for toggling education section of my resume
+education.addEventListener('click', openEducation)
+// for toggling experience section of my resume
+experience.addEventListener('click', openExperience)
